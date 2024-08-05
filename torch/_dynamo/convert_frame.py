706b807dbb75ba3bea7dfa5ae4e0814421afe235
laithsakka <lsakka@meta.com>
2024-08-05 09:06:32 -0700

    introduce regression to be detected
    ghstack-source-id: 5f85634c0e6c6137cbfa8a259fa984801f8d97d1
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/132215

--- torch/_dynamo/convert_frame.py
+++ torch/_dynamo/convert_frame.py
@@ -644,7 +644,10 @@ def _compile(
         nonlocal dynamo_time_before_restart
         nonlocal restart_reasons
         last_attempt_start_time = start_time = time.time()
-
+        j =0
+        for i in range(100):
+            j=j*i
+        print(j)    
         def log_bytecode(
             prefix: str, name: str, filename: str, line_no: int, code: CodeType
         ) -> None:
